c97e8c035249e36992d6bcd64b81fdd5f86f7880
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-03-15 19:48:29 +0300

    add export function

---
+++ src/spiralMatrix.js
@@ -0,0 +1,40 @@
+exports.spiralMatrix = function (dimention = 5, initialValue = 0) {
+  let dy = 0
+  let dx = -1
+  let value = initialValue - 1
+  const seqTotal = (2 * dimention - 2) / 2
+  let seqDigitsTotal = dimention
+  let seqNumber = 0
+  let seqSign = -1
+
+  // Create the matrix nxn
+  let n = dimention
+  let matrix = []
+  while (n) matrix[--n] = []
+
+  // Fill in the first row (index 0)
+  for (let i = 0; i < dimention; i++) {
+    matrix[dy][++dx] = ++value
+  }
+
+  // Find the row and column numbers and set the values.
+  for (let i = 1; i <= seqTotal; i++) {
+    seqSign = -seqSign
+    seqDigitsTotal--
+    for (let i = 1; i <= 2; i++) {
+      seqNumber++
+      for (let i = 0; i < seqDigitsTotal; i++) {
+        if (seqNumber % 2 != 0) {
+          dy = dy + 1 * seqSign
+        } else {
+          dx = dx - 1 * seqSign
+        }
+        value++
+        matrix[dy][dx] = value
+      }
+    }
+  }
+  return matrix
+}
+
+
