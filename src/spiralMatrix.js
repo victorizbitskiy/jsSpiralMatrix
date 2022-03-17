62423383a5ff241ceaf82b0a20a540aa50200a8e
victorizbitskiy <victor.izbitskiy@yandex.ru>
2022-03-17 08:40:55 +0300

    changed start column coordinates

--- src/spiralMatrix.js
+++ src/spiralMatrix.js
@@ -1,8 +1,8 @@
 exports.spiralMatrix = function (dimention = 5, initialValue = 0) {
   let dy = 0
-  let dx = -1
+  let dx = 0
   let value = initialValue - 1
-  const seqTotal = (2 * dimention - 2) / 2
+  const halfSeqTotal = (2 * dimention - 2) / 2
   let seqDimention = dimention
   let seqNumber = 0
   let signCoef = -1
@@ -14,14 +14,15 @@ exports.spiralMatrix = function (dimention = 5, initialValue = 0) {
 
   // Fill in the first row (index 0)
   for (let i = 0; i < dimention; i++) {
-    matrix[dy][++dx] = ++value
+    matrix[dy][dx++] = ++value
   }
 
   // Find the row and column numbers and set the values.
-  for (let i = 1; i <= seqTotal; i++) {
+  dx--
+  for (let i = 0; i < halfSeqTotal; i++) {
     signCoef = - signCoef
     seqDimention--
-    for (let i = 1; i <= 2; i++) {
+    for (let i = 0; i < 2; i++) {
       seqNumber++
       for (let i = 0; i < seqDimention; i++) {
         if (seqNumber % 2 != 0) {
